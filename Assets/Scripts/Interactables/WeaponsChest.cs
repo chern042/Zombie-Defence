1df92b673b10a0d12ff2f2d749993e9def10faa1
Carlos Hernandez <chern042@uottawa.ca>
2023-12-15 09:12:39 -0500

    Added New Asset Pack

--- Assets/Scripts/Interactables/WeaponsChest.cs
+++ Assets/Scripts/Interactables/WeaponsChest.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem.OnScreen;
+using UnityEngine.UI;
 
-public class WeaponsChest : MonoBehaviour
+public class WeaponsChest : Interactable
 {
+    private Animator animator;
+
+    private int[] itemIndexList;
+
+    private GameObject[] itemList;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //test
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+
+    protected override void OnLook()
+    {
+
+        interactButton.GetComponentInChildren<TextMeshProUGUI>().text = "Buy";
+        interactButton.GetComponentInChildren<TextMeshProUGUI>().enabled = false;
+        interactButton.GetComponent<Image>().enabled = false;
+        interactButton.GetComponent<OnScreenButton>().enabled = false;
+        promptMessage = "Purchase a weapon (2000).";
+    }
+
+    protected override void Interact()
+    {
+        base.Interact();
+    }
+
+    public override void CancelInteract()
+    {
+        base.CancelInteract();
+    }
+
+    public override void OnLookOff()
+    {
+
     }
 }
