ab439cdfd8ba5e80ce8394d9923820047426a5f6
Carlos Hernandez <chern042@uottawa.ca>
2024-02-03 14:15:06 -0500

    Got both joysticks to appear and function individually.
    However. when used together, the second joystick does not move. Also, the buttons are unavailable to press when a joystick is activated.

--- Assets/Joysticks.cs
+++ Assets/Joysticks.cs
@@ -18,8 +18,12 @@ public class Joysticks : MonoBehaviour
     [SerializeField]
     private GameObject joystickLeft;
 
+    [SerializeField]
+    private GameObject joystickRight;
 
-    private bool joyStickOpen;
+
+    private bool joyStickLeftOpen;
+    private bool joyStickRightOpen;
     private CanvasHitDetector hitDetector;
     private TouchStates touchState;
     private float screenWidth;
@@ -27,9 +31,10 @@ public class Joysticks : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        joyStickOpen = false;
+        joyStickLeftOpen = false;
+        joyStickRightOpen = false;
         hitDetector = GetComponent<CanvasHitDetector>();
-        screenWidth = Screen.width;
+        screenWidth = Screen.width > Screen.height ? Screen.width : Screen.height;
     }
 
     // Update is called once per frame
@@ -38,14 +43,15 @@ public class Joysticks : MonoBehaviour
     }
 
 
-    public void OnTryClick(InputAction.CallbackContext context)
+    public void OnTryEnableJoystickOne(InputAction.CallbackContext context)
     {//Instead of button... try pass-through?
        // Debug.Log("phase: " + context.phase);
 
         Debug.Log("EVENT SYSTEM TEST: "+ hitDetector.IsPointerOverUILayer());
+        Debug.Log("******************ON TRY ENABLE LEFT JOYSTICK*************");
 
 
-       if (context.valueType == typeof(TouchState))
+        if (context.valueType == typeof(TouchState))
         {
             object touchObj = context.ReadValueAsObject();
 
@@ -56,46 +62,174 @@ public class Joysticks : MonoBehaviour
 
             touchState = (TouchStates)(int)touch["phaseId"];
 
-
-
-
-
             Debug.Log("JSON OBJ TOUCH: " + touch.ToString());
 
-
-            if (touchState == TouchStates.Touch && !joyStickOpen)
+            if (touchState == TouchStates.Touch)
             {
-                if (!hitDetector.IsPointerOverUILayer() && touchPosition.x < screenMidPointX)
+                if (!hitDetector.IsPointerOverUILayer() && touchPosition.x < screenMidPointX && !joyStickLeftOpen)
                 {
-                    Debug.Log("STARTED");
+                    Debug.Log("STARTED LEFT");
                     joystickLeft.transform.position = new Vector3((float)touch["position"]["x"], (float)touch["position"]["y"], 0);
                     joystickLeft.gameObject.SetActive(true);
-                    joyStickOpen = true;
+                    joyStickLeftOpen = true;
+                }
+                else if (!hitDetector.IsPointerOverUILayer() && touchPosition.x > screenMidPointX && !joyStickRightOpen)
+                {
+                    Debug.Log("STARTED RIGHT");
+                    joystickRight.transform.position = new Vector3((float)touch["position"]["x"], (float)touch["position"]["y"], 0);
+                    joystickRight.gameObject.SetActive(true);
+                    joyStickRightOpen = true;
                 }
             }
-            else if (touchState == TouchStates.TouchEnded && joyStickOpen)
+            else if (touchState == TouchStates.TouchEnded)
             {
-                Debug.Log("CANCELED: " + joystickLeft.gameObject.GetComponentInChildren<OnScreenStick>().gameObject.name);
-                joystickLeft.gameObject.SetActive(false);
-                joystickLeft.gameObject.GetComponentInChildren<OnScreenStick>().gameObject.transform.localPosition = new Vector3(0, 0, 0);
-                joyStickOpen = false;
+                if(touchPosition.x < screenMidPointX && joyStickLeftOpen)
+                {
+                    Debug.Log("CANCELED LEFT");
+                    joystickLeft.gameObject.SetActive(false);
+                    joystickLeft.gameObject.GetComponentInChildren<OnScreenStick>().gameObject.transform.localPosition = new Vector3(0, 0, 0);
+                    joyStickLeftOpen = false;
+                }
+                else if(touchPosition.x > screenMidPointX && joyStickRightOpen)
+                {
+                    Debug.Log("CANCELED RIGHT");
+                    joystickRight.gameObject.SetActive(false);
+                    joystickRight.gameObject.GetComponentInChildren<OnScreenStick>().gameObject.transform.localPosition = new Vector3(0, 0, 0);
+                    joyStickRightOpen = false;
+                }
             }
             else
             {
                 touchState = TouchStates.NoTouch;
             }
-
         }
         else
         {
             Debug.Log("UNKNOWN VALUE TYPE: " + context.valueType);
-
             Debug.Log("READ AS OBJECT: " + context.ReadValueAsObject());
-
         }
-
-
-
     }
+
+    public void OnTryEnableJoystickTwo(InputAction.CallbackContext context)
+    {//Instead of button... try pass-through?
+     // Debug.Log("phase: " + context.phase);
+
+        Debug.Log("EVENT SYSTEM TEST: " + hitDetector.IsPointerOverUILayer());
+        Debug.Log("******************ON TRY ENABLE LEFT JOYSTICK*************");
+
+
+        if (context.valueType == typeof(TouchState))
+        {
+            object touchObj = context.ReadValueAsObject();
+
+            JObject touch = JObject.Parse(JsonUtility.ToJson(touchObj));
+
+            Vector2 touchPosition = new Vector2((float)touch["position"]["x"], (float)touch["position"]["y"]);
+            float screenMidPointX = screenWidth / 2f;
+
+            touchState = (TouchStates)(int)touch["phaseId"];
+
+            Debug.Log("JSON OBJ TOUCH: " + touch.ToString());
+
+            if (touchState == TouchStates.Touch)
+            {
+                if (!hitDetector.IsPointerOverUILayer() && touchPosition.x < screenMidPointX && !joyStickLeftOpen)
+                {
+                    Debug.Log("STARTED LEFT");
+                    joystickLeft.transform.position = new Vector3((float)touch["position"]["x"], (float)touch["position"]["y"], 0);
+                    joystickLeft.gameObject.SetActive(true);
+                    joyStickLeftOpen = true;
+                }
+                else if (!hitDetector.IsPointerOverUILayer() && touchPosition.x > screenMidPointX && !joyStickRightOpen)
+                {
+                    Debug.Log("STARTED RIGHT");
+                    joystickRight.transform.position = new Vector3((float)touch["position"]["x"], (float)touch["position"]["y"], 0);
+                    joystickRight.gameObject.SetActive(true);
+                    joyStickRightOpen = true;
+                }
+            }
+            else if (touchState == TouchStates.TouchEnded)
+            {
+                if (touchPosition.x < screenMidPointX && joyStickLeftOpen)
+                {
+                    Debug.Log("CANCELED LEFT");
+                    joystickLeft.gameObject.SetActive(false);
+                    joystickLeft.gameObject.GetComponentInChildren<OnScreenStick>().gameObject.transform.localPosition = new Vector3(0, 0, 0);
+                    joyStickLeftOpen = false;
+                }
+                else if (touchPosition.x > screenMidPointX && joyStickRightOpen)
+                {
+                    Debug.Log("CANCELED RIGHT");
+                    joystickRight.gameObject.SetActive(false);
+                    joystickRight.gameObject.GetComponentInChildren<OnScreenStick>().gameObject.transform.localPosition = new Vector3(0, 0, 0);
+                    joyStickRightOpen = false;
+                }
+            }
+            else
+            {
+                touchState = TouchStates.NoTouch;
+            }
+        }
+        else
+        {
+            Debug.Log("UNKNOWN VALUE TYPE: " + context.valueType);
+            Debug.Log("READ AS OBJECT: " + context.ReadValueAsObject());
+        }
+    }
+
+    //public void OnTryEnableRightJoystick(InputAction.CallbackContext context)
+    //{//Instead of button... try pass-through?
+    // // Debug.Log("phase: " + context.phase);
+
+    //    Debug.Log("******************ON TRY ENABLE RIGHT JOYSTICK*************");
+    //    Debug.Log("CONTEXT TYPE: " + context.GetType());
+
+    //    if (context.valueType == typeof(TouchState))
+    //    {
+    //        object touchObj = context.ReadValueAsObject();
+
+    //        JObject touch = JObject.Parse(JsonUtility.ToJson(touchObj));
+
+    //        Vector2 touchPosition = new Vector2((float)touch["position"]["x"], (float)touch["position"]["y"]);
+    //        float screenMidPointX = screenWidth / 2f;
+
+    //        touchState = (TouchStates)(int)touch["phaseId"];
+
+    //        Debug.Log("JSON OBJ TOUCH: " + touch.ToString());
+
+    //        Debug.Log("TEST TOUCH POSITION BOOL: "+(touchPosition.x > screenMidPointX));
+    //        Debug.Log("TEST TOUCH POSITION: " + touchPosition);
+    //        Debug.Log("TEST SCREEN MIDPOINT: " + screenMidPointX);
+    //        Debug.Log("TEST TOUCH STATE: " + touchState); ;
+    //        Debug.Log("TEST JOYSTICK RIGHT OPEN: " + joyStickRightOpen);
+
+    //        if (touchState == TouchStates.Touch && !joyStickRightOpen)
+    //        {
+    //            if (!hitDetector.IsPointerOverUILayer() && touchPosition.x > screenMidPointX)
+    //            {
+    //                Debug.Log("STARTED");
+    //                joystickRight.transform.position = new Vector3((float)touch["position"]["x"], (float)touch["position"]["y"], 0);
+    //                joystickRight.gameObject.SetActive(true);
+    //                joyStickRightOpen = true;
+    //            }
+    //        }
+    //        else if (touchState == TouchStates.TouchEnded && joyStickRightOpen)
+    //        {
+    //            Debug.Log("CANCELED: " + joystickRight.gameObject.GetComponentInChildren<OnScreenStick>().gameObject.name);
+    //            joystickRight.gameObject.SetActive(false);
+    //            joystickRight.gameObject.GetComponentInChildren<OnScreenStick>().gameObject.transform.localPosition = new Vector3(0, 0, 0);
+    //            joyStickRightOpen = false;
+    //        }
+    //        else
+    //        {
+    //            touchState = TouchStates.NoTouch;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("UNKNOWN VALUE TYPE: " + context.valueType);
+    //        Debug.Log("READ AS OBJECT: " + context.ReadValueAsObject());
+    //    }
+    //}
 
 }
--- Assets/Scripts/SceneHelper.cs
+++ Assets/Scripts/SceneHelper.cs
@@ -14,49 +14,5 @@ public static class SceneHelper
         }
     }
 
-    public static Dictionary<string, TValue> ToDictionary<TValue>(object obj)
-    {
-        //var json = JsonConvert.SerializeObject(obj);
-        var json = JsonUtility.ToJson(obj);
-        //var dictionary = JsonConvert.DeserializeObject<Dictionary<string, TValue>>(json);
-        var dictionary = JsonUtility.FromJson<Dictionary<string, TValue>>(json);
-        return dictionary;
-    }
-}
 
-
-public static class ObjectToDictionaryHelper
-{
-    public static IDictionary<string, object> ToDictionary(this object source)
-    {
-        return source.ToDictionary<object>();
-    }
-
-    public static IDictionary<string, T> ToDictionary<T>(this object source)
-    {
-        if (source == null)
-            ThrowExceptionWhenSourceArgumentIsNull();
-
-        var dictionary = new Dictionary<string, T>();
-        foreach (PropertyDescriptor property in TypeDescriptor.GetProperties(source))
-            AddPropertyToDictionary<T>(property, source, dictionary);
-        return dictionary;
-    }
-
-    private static void AddPropertyToDictionary<T>(PropertyDescriptor property, object source, Dictionary<string, T> dictionary)
-    {
-        object value = property.GetValue(source);
-        if (IsOfType<T>(value))
-            dictionary.Add(property.Name, (T)value);
-    }
-
-    private static bool IsOfType<T>(object value)
-    {
-        return value is T;
-    }
-
-    private static void ThrowExceptionWhenSourceArgumentIsNull()
-    {
-        throw new ArgumentNullException("source", "Unable to convert object to a dictionary. The source object is null.");
-    }
 }
